5e8b364cf382ac1197ed16923ab32c685f264c42
chriskeraly <chriskeraly@gmail.com>
2018-09-14 15:56:37 +0200

    docs debug

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 print os.path.abspath('..')
+import matplotlib
+matplotlib.use('Agg')
 
 # -- Project information -----------------------------------------------------
 
